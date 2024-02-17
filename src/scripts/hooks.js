c23f029e6a62c990d16c367e4b79eaedd5f89a09
Stexinator <hurricane90@gmx.de>
2024-02-17 14:31:51 +0100

    fix code fuckup

--- src/scripts/hooks.js
+++ src/scripts/hooks.js
@@ -27,12 +27,29 @@ import Settings from "./settings.js";
          else {
             ui.SR5HUD.render(true);
          }
+      });
+ });
+
+    Hooks.on('renderApplication', async function(actor, html) {
+      html.find('.sr5-hud-actors-box').on('click', async ev => {
+         (await fromUuid(ev.currentTarget.attributes['actor-uuid'].value)).sheet?.render(true)
+      })
+   });
+
+   Hooks.on('renderSceneControls', (controls, html) => {
+    html.find('[data-tool="sr5-hud"]').on('click', (event) => {
+         event.preventDefault();
+         if(ui.SR5HUD.rendered) {
+            ui.SR5HUD.close();
+         }
+         else {
+            ui.SR5HUD.render(true);
+         }
 
       });
  });
 
     Hooks.on('renderApplication', async function(actor, html) {
-
       html.find('.sr5-hud-actors-box').on('click', async ev => {
          (await fromUuid(ev.currentTarget.attributes['actor-uuid'].value)).sheet?.render(true)
       })
