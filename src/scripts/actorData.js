554b979bae8e1ed661465d939f5d20cb6c23e20b
Shadows_eye <71041904+shadows-eye@users.noreply.github.com>
2024-02-25 19:09:55 +0100

    improvement_hud_layout
    more data and hud improvement

--- src/scripts/actorData.js
+++ src/scripts/actorData.js
@@ -25,7 +25,10 @@ export default class ActorData{
                 weapons: ActorData.getWeapons(actor),
                 spells: ActorData.getSpells(actor),
                 adeptPowers: ActorData.getAdeptPowers(actor),
-                actions: ActorData.getActions(actor)
+                actions: ActorData.getActions(actor),
+                contacts: ActorData.getContacts(actor),
+                qualitys: ActorData.getQualitys(actor),
+                /*matrixs: ActorData.getMatrix(actor)*/
             }
         }
     }
@@ -73,21 +76,69 @@ export default class ActorData{
     }
 
     static getWeapons(actor) {
-        return actor.items.filter(item => item.type == "weapon").filter(weapon => weapon.system.technology.equipped)
+        let unsorted = actor.items.filter(item => item.type == "weapon" && item.system.technology.equipped);
+
+        let weapons = {
+            rangedWeapons: unsorted.filter(weapon => weapon.system.action.test === "RangedAttackTest"),
+            meleeWeapons: unsorted.filter(weapon => weapon.system.action.test === "MeleeAttackTest")
+        };
+        return weapons;
     }
 
     static getSpells(actor) {
-        let unsorted = actor.items.filter(item => item.type == "spell")
-
+        let unsorted = actor.items.filter(item => item.type == "spell");
+    
         let spells = {
             combat: unsorted.filter(spell => spell.system.category === "combat"),
             detection: unsorted.filter(spell => spell.system.category === "detection"),
             manipulation: unsorted.filter(spell => spell.system.category === "manipulation"),
             illusion: unsorted.filter(spell => spell.system.category === "illusion"),
-            health: unsorted.filter(spell => spell.system.category === "health")
-        }
+            health: unsorted.filter(spell => spell.system.category === "health"),
+            // Directly compute hasSpells based on the presence of spells in any category
+            hasSpells: false // Default to false, will update next
+        };
+    
+        // Update hasSpells based on the presence of spells in any category
+        spells.hasSpells = Object.values(spells).some(category => Array.isArray(category) && category.length > 0);
+    
         return spells;
     }
+    static getContacts(actor) {
+        return actor.items.filter(item => item.type == "contact")
+    }
+    static getQualitys(actor) {
+        let unsorted = actor.items.filter(item => item.type == "quality");
+    
+        let qualitys = {
+            negative: unsorted.filter(quality => quality.system.type === "negative"),
+            positive: unsorted.filter(quality => quality.system.type === "positive"),
+            // Initially set hasQualities to false; this will be updated below
+            hasQualitys: false
+        };
+    
+        // Update hasQualities based on the presence of qualities in either category
+        qualitys.hasQualitys = qualitys.negative.length > 0 || qualitys.positive.length > 0;
+    
+        return qualitys;
+    }
+    //Matrix Section still needs work
+    /*
+    static getMatrix(actor) {
+        let unsortedProgramms = actor.items.filter(item => item.type == "programm");
+    
+        let matrixs = {
+            negative: unsortedProgramms.filter(programm => programm.system.type === "negative"),
+            positive: unsortedProgramms.filter(programm => programm.system.type === "positive"),
+            // Initially set hasProgramms to false; this will be updated below
+            hasProgramms: false
+        };
+    
+        // Update hasProgramms based on the presence of programms in either category
+        programms.hasProgramms = programms.negative.length > 0 || programms.positive.length > 0;
+    
+        return matrixs;
+    }
+    */
 
     static getAdeptPowers(actor) {
         return actor.items.filter(item => item.type == "adept_power").filter(power => power.system.type != "passive")
@@ -115,5 +166,4 @@ export default class ActorData{
 
          return skillsWithRating
     }
-
 }
